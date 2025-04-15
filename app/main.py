e0951072eff03b9319d3a025661127a805fdd5ff
joshua <joshua92y@gmail.com>
2025-04-15 13:11:02 +0900

    옵션스 전역설정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request
 from app.api import analyze,storage,convert,preset,history
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -18,3 +19,17 @@ app.add_middleware(
     allow_methods=["*"],  # ["POST", "GET", "OPTIONS"]만 설정해도 OK
     allow_headers=["*"],
 )
+
+
+
+@app.options("/{rest_of_path:path}")
+async def preflight_global(request: Request, rest_of_path: str):
+    return JSONResponse(
+        status_code=200,
+        content={"message": "Preflight OK"},
+        headers={
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
+            "Access-Control-Allow-Headers": "*",
+        }
+    )
